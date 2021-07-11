dc429771a17389ecf370f14bf3b02e43248ed2e3
Gaëtan <gaerisson2k9@live.fr>
2021-07-11 22:22:15 +0200

    -

---
+++ code/weapons/Haxgun.cs
@@ -0,0 +1,59 @@
+﻿using Sandbox;
+
+[Library( "weapon_haxgun", Title = "HaxGun", Spawnable = true )]
+partial class Haxgun : Weapon
+{
+	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+	public override float PrimaryRate => 15.0f;
+	public override float SecondaryRate => 1.0f;
+	public override float ReloadTime => 5.0f;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		return base.CanPrimaryAttack() && Input.Pressed( InputButton.Attack1 );
+	}
+
+	public override void AttackPrimary()
+	{
+		TimeSincePrimaryAttack = 0;
+		TimeSinceSecondaryAttack = 0;
+		
+		(Owner as AnimEntity)?.SetAnimBool( "b_attack", true );
+
+		ShootEffects();
+		PlaySound( "rust_pistol.shoot" );
+
+		ShootBullet( 0.05f, 1.5f, 9.0f, 3.0f );
+	}
+
+	public override void AttackSecondary()
+	{
+
+	}
+
+	[ClientRpc]
+	protected override void ShootEffects()
+	{
+		Host.AssertClient();
+
+		Particles.Create( "particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle" );
+		Particles.Create( "particles/pistol_ejectbrass.vpcf", EffectEntity, "ejection_point" );
+
+		ViewModelEntity?.SetAnimBool( "fire", true );
+		CrosshairPanel?.CreateEvent( "fire" );
+	}
+	
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		anim.SetParam( "holdtype", 2 ); // TODO this is shit
+		anim.SetParam( "aimat_weight", 1.0f );
+	}
+}
