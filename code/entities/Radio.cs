65b62ffb5085a4d06ea779f94714d5aee71dcdb3
Gaëtan <gaerisson2k9@live.fr>
2021-07-11 23:55:51 +0200

    -

---
+++ code/entities/Radio.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using System;
+
+[Library( "ent_radio", Title = "Radio", Spawnable = true )]
+public partial class RadioEntity : Prop, IUse
+{
+
+	public bool Enable { get; set; } = true;
+	private Sound RadioSnd;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		CheckSnd();
+		SetModel( "models/heavybox/radio.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+	}
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		if ( user is Player player )
+		{
+			CheckSnd();
+		}
+
+		return false;
+	}
+
+	public void CheckSnd()
+	{
+		if(Enable==true){
+			Enable=false;
+			RadioSnd = PlaySound( "radio.radio_music" );
+		}else{
+			Enable=true;
+			RadioSnd.Stop();
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		RadioSnd.Stop();
+		base.OnDestroy();
+	}
+}
